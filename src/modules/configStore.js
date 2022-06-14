571b6614ba5d6ddc2920bf2d86a21b9fb9e9636f
hayeoooon <kimma0514@gmail.com>
2022-06-14 12:09:16 +0900

    comment reducer 추가

--- src/modules/configStore.js
+++ src/modules/configStore.js
@@ -2,10 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import user from '../modules/redux/user';
 import haedal from '../modules/redux/haedal';
+import comment from '../modules/redux/comment';
 import {createBrowserHistory} from 'history';
 
 const customHistory = createBrowserHistory();
-const rootReducer = combineReducers({user, haedal});
+const rootReducer = combineReducers({user, haedal, comment});
 const enhancer = applyMiddleware(
   thunk.withExtraArgument({ history: customHistory }),
 )
